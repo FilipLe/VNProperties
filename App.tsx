b33e28a213d1524707b9a013b9545b38e9509ac0
FilipLe <48429871+FilipLe@users.noreply.github.com>
2024-05-18 10:43:02 +0200

    [feat] integrate basic maps onto screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
+import { NavigationContainer, NavigationProp } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import React from 'react'; // Ensure React is imported when using JSX in TypeScript
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import MapView from 'react-native-maps';
+import React from 'react'; 
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Settings from './src/pages/Settings';
 
-export default function App(): JSX.Element {
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }: { navigation: NavigationProp<any>}): JSX.Element {
   return (
     <View style={styles.container}>
       {/* Search Bar */}
       <View style={styles.searchBarContainer}>
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+          <MaterialIcons name="menu" size={30} color="#000" />
+        </TouchableOpacity>
         <TextInput
           style={styles.searchBar}
           placeholder="Wpisz lokalizację"
@@ -15,37 +25,53 @@ export default function App(): JSX.Element {
 
       {/* Map */}
       <View style={styles.mapContainer}>
-        {/* Empty container for future map implementation */}
+        <MapView style = {{width:'100%', height:'100%'}}></MapView>
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
+  // main home screen container
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+
   searchBarContainer: {
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     width: '100%',
     padding: 10,
-    paddingTop: 60,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
+
   searchBar: {
     height: 40,
-    width: '90%',
+    flex: 1,
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
     paddingLeft: 10,
+    marginLeft: 10, 
   },
+
   mapContainer: {
     flex: 1,
     width: '100%',
