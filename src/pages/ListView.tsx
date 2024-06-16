2e7b4f99e038486a8c348bffb65eca28f5961a16
FilipLe <48429871+FilipLe@users.noreply.github.com>
2024-06-16 20:16:50 +0200

    [feat] added front end star icons for favorites in List View

--- src/pages/ListView.tsx
+++ src/pages/ListView.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text, ScrollView, StyleSheet, Image, Pressable } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { locations } from "../locations";
 import { Location } from "../components/Location";
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from "../services/AppNavigator";
+import Icon from 'react-native-vector-icons/AntDesign';
 
 type ListViewNavigationProp = StackNavigationProp<RootStackParamList, 'ListView'>;
 
 export default function ListView(): JSX.Element {
     const navigation = useNavigation<ListViewNavigationProp>();
+
+    const [starredItems, setStarredItems] = useState(new Set<number>());
+    const toggleStarred = (index: number) => {
+        const updatedStarredItems = new Set(starredItems);
+        if (starredItems.has(index)) {
+            updatedStarredItems.delete(index);
+        } else {
+            updatedStarredItems.add(index);
+        }
+        setStarredItems(updatedStarredItems);
+    };
+    
     return (
         <ScrollView>
             {locations.map((location: Location, index: number) => (
@@ -35,6 +48,9 @@ export default function ListView(): JSX.Element {
                     <Text>Rooms: {location.rooms}</Text>
                     <Text>Latitude: {location.location.latitude}</Text>
                     <Text>Longitude: {location.location.longitude}</Text>
+                    <Pressable style={styles.starButton} onPress={() => toggleStarred(index)}>
+                        {starredItems.has(index) ? <Icon name="star" size={20} color="gold" /> : <Icon name="staro" size={20} color="black" />}
+                    </Pressable>
                 </Pressable>
             ))}
         </ScrollView>
@@ -61,5 +77,11 @@ const styles = StyleSheet.create({
         height: 1,
         backgroundColor: '#ddd',
         marginVertical: 10,
+    },
+    starButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        fontSize: 20,
     },
 });
